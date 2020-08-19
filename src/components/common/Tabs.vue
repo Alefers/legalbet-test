8bdbccc8d0122a3366a242847090a7af41c561a1
Alfers <alefersxaoc@gmail.com>
2020-08-19 19:15:44 +0300

    time to go to bed...

---
+++ src/components/common/Tabs.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul class="tabs">
+        <li
+                v-for="(tab, idx) in tabs"
+                class="tabs__item"
+                :class="{'tabs__item_active': tab.active}"
+                :key="idx"
+                @click="$emit('changetab', idx)"
+        >
+            {{tab.text}}
+            <span class="tabs-item-count" v-if="tab.hasOwnProperty('count') && tab.count !== null" v-html="tab.count" />
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: {
+        tabs: {
+            type: Array,
+            default: () => []
+        }
+    }
+}
+</script>
--- src/components/matches/Matches.vue
+++ src/components/matches/Matches.vue
@@ -4,11 +4,7 @@
             <div class="row__col row__col_main">
                 <div class="box box_white">
                     <div class="box__header">
-                        <ul class="tabs">
-                            <li class="tabs__item">
-
-                            </li>
-                        </ul>
+                        <Tabs :tabs="tabs" @changetab="goToTab"/>
                     </div>
                     <div class="box__body">
                         <h1>Все матчи</h1>
@@ -25,10 +21,44 @@
 </template>
 
 <script>
+import Tabs from "../common/Tabs";
+
 export default {
+    components: {Tabs},
     data: () => {
         return {
-            tabs: []
+            tabs: [
+                {
+                    text: 'На сегодня',
+                    count: 13,
+                    active: false
+                },
+                {
+                    text: 'Все матчи',
+                    count: 87,
+                    active: true
+                },
+                {
+                    text: 'Составы',
+                    active: false
+                },
+                {
+                    text: 'Новости',
+                    active: false
+                },
+                {
+                    text: 'Ставки и прогнозы',
+                    active: false
+                },
+            ]
+        }
+    },
+    methods: {
+        goToTab(idx) {
+            this.tabs = this.tabs.map((tab, index) =>  {
+                tab.active = index === idx;
+                return tab;
+            });
         }
     }
 }
